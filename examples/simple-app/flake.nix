3f505c4b455286e4fd389ba9fb96eccbc4467a78
felix <zen9.felix@gmail.com>
2024-04-30 19:07:44 +0800

    Test with 3.3.1

--- examples/simple-app/flake.nix
+++ examples/simple-app/flake.nix
@@ -46,7 +46,7 @@
         gemConfig = { };
 
         # See available versions here: https://github.com/bobvanderlinden/nixpkgs-ruby/blob/master/ruby/versions.json
-        ruby = pkgs."ruby-3.2";
+        ruby = pkgs."ruby-3.3.1";
 
         bundixcli = bundix.packages.${system}.default;
       in
